dbc7d5c8d23b531bbd6acebb0aaacfe041dfe289
birdbird1117 <dapeng.han.1989@gmail.com>
2020-04-01 12:08:22 +0200

    148 more responsive ajustments

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -10,7 +10,7 @@ const toolbar = (props) => {
             <div>MENU</div>
             <div className={classes.Logo}><Logo /></div>
 
-            <nav>
+            <nav className={classes.DesktopOnly}>
                 <NavigationItems />
             </nav>
         </header>
